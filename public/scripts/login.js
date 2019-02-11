12a8dc56547ab423a8302d31169ec0c35b931bde
Preston Hager <preston@hagerfamily.com>
2019-02-10 17:24:11 -0800

    Updated Items

--- public/scripts/login.js
+++ public/scripts/login.js
@@ -1,7 +1,7 @@
 function onSignIn(googleUser) {
   var profile = googleUser.getBasicProfile();
   var id = googleUser.getAuthResponse().id_token;
-  var new_profile = {"id": id, "client_id": profile.getId(), "name": profile.getName(), "image": profile.getImageUrl(), "email": profile.getEmail()};
+  new_profile = {"id": id, "client_id": profile.getId(), "name": profile.getName(), "image": profile.getImageUrl(), "email": profile.getEmail()};
   window.localStorage.setItem("profile", JSON.stringify(new_profile));
   // sign out the user so that it doesn't auto sign-in again.
   var auth2 = gapi.auth2.getAuthInstance();
@@ -15,15 +15,10 @@ function onFailure(error) {
   console.log(error);
 };
 $(document).ready(function(){
-  var profile = window.localStorage.getItem("profile");
-  if (!profile) {
+  if (!window.localStorage.getItem("profile")) {
     window.localStorage.setItem("profile", "{}");
     $("div.player-login-parent").show();
-  } else if (profile == "{}") {
+  } else if (window.localStorage.getItem("profile") == "{}") {
     $("div.player-login-parent").show();
-  } else {
-    var form = $('<form action="/dashboard" method="post" />').append($('<input style="display:none" type="text" name="profile" />').val(JSON.stringify(profile)));
-    $(document.body).append(form);
-    form.submit();
   }
 });
